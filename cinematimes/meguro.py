8d2aee2daded75e0becd564193ea34419ec3411e
ben thompson <ben97thompson@gmail.com>
2022-05-09 19:53:28 +0900

    cleaning japanese from text

--- cinematimes/meguro.py
+++ cinematimes/meguro.py
@@ -23,9 +23,14 @@ def movie_extract(soup):
     '''
     extracts movie text from relevant html soup
     '''
-    pattern = r"タイトル☆-->(.*)<!--☆ここまで"
+    engpattern = r'[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]' #unicode for chinese/jp chars
+    jppattern = r"タイトル☆-->(.*)<!--☆ここまで"
+
     engtitle = soup.find('span', class_='text_small').text
-    jptitle = re.findall(pattern, str(soup))[0].replace('\u3000', ' ')
+    engtitle = re.sub(pattern,'', engtitle)
+
+    jptitle = re.findall(jppattern, str(soup))[0].replace('\u3000', ' ')
+
     movie_dict = {'engtitle':engtitle, 'jptitle':jptitle}
     return(movie_dict)
 
